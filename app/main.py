417959bd20f74e752e275a08413ea901d2af931d
probablyshabina <it21170034@my.sliit.lk>
2025-03-18 23:31:23 +0530

    adds the cron job

--- app/main.py
+++ app/main.py
@@ -2,11 +2,14 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.trace_writer import run_trace_writer
+from apscheduler.schedulers.background import BackgroundScheduler
+
+scheduler = BackgroundScheduler()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Starting up: ")
-    run_trace_writer()
     
     yield 
 
@@ -26,3 +29,22 @@ app.add_middleware(
 @app.get("/health")
 async def root():
     return {"message": "Trace Writer Server is running"}
+
+@app.post("/start")
+def start_cron():
+    global scheduler
+    if not scheduler.running:
+        scheduler.add_job(run_trace_writer, 'interval', seconds=10, id='cron_run_trace_writer')
+        scheduler.start()
+        return {"message": "Cron job to write traces started."}
+    return {"message": "Cron job to write traces already running."}
+
+@app.post("/stop")
+def stop_cron():
+    global scheduler
+    if scheduler.running:
+        scheduler.remove_job('cron_run_trace_writer')
+        scheduler.shutdown()
+        return {"message": "Cron job to write traces stopped."}
+    return {"message": "Cron job to write traces already stopped."}
+
